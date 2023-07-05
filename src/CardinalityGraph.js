75d6770f30b812914712ecf554fa484b6600bb8a
Chat-Wane <Chat-Wane@users.noreply.github.com>
2023-07-05 10:43:20 +0000

    deploy: 324dc7f34641d0c4e41cbaac57c607a83e2caf6f

---
+++ src/CardinalityGraph.js
@@ -0,0 +1,70 @@
+import {Chart, registerables} from "chart.js";
+Chart.register(...registerables);
+
+
+export class CardinalityGraph {
+    
+    constructor(data, viewPoint) {
+        let canvas = document.createElement("canvas");
+        viewPoint.appendChild(canvas);
+        
+        new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: data.map(e => e.x),
+                datasets: [
+                    { label: '',
+                      backgroundColor: 'rgba(235, 203, 139,  0.3)',
+                      type: 'line',
+                      data: data.map(e => e.y + e.ci),
+                      fill: false,
+                      pointRadius: 0
+                    },
+                    { label: '',
+                      backgroundColor: 'rgba(235, 203, 139,  0.3)',
+                      type: 'line',
+                      data: data.map(e => e.y - e.ci),
+                      fill: '-1',
+                      pointRadius: 0 },
+                    { label: 'Estimated cardinality',
+                      type: 'line',
+                      backgroundColor: 'rgba(163, 190, 140,  1)',
+                      data: data.map(e => e.y),  
+                      borderWidth: 5,
+                    }
+                ]
+            },
+            options: {
+                animation: {
+                    duration: 0
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Estimated " + data[data.length-1].y + " ± "+data[data.length-1].ci + " elements",
+                    },
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    // y: { beginAtZero: true },
+                    x: {
+                        title:{
+                            display:true,
+                            text: "Number of random walks"
+                        },
+                        beginAtZero: true
+                    },
+                    y: {
+                        title:{
+                            display:true,
+                            text: "Cardinality estimation"
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+

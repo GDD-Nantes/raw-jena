e5f8908ff4c4772f71555aa6d05e1eea09ba3743
chat-wane <grumpy.chat.wane@gmail.com>
2024-03-29 12:02:04 +0100

    todo groupby

--- rawer/src/main/java/fr/gdd/sage/rawer/RawerOpExecutor.java
+++ rawer/src/main/java/fr/gdd/sage/rawer/RawerOpExecutor.java
@@ -13,15 +13,14 @@ import fr.gdd.sage.sager.pause.Save2SPARQL;
 import fr.gdd.sage.sager.resume.BGP2Triples;
 import org.apache.jena.atlas.iterator.Iter;
 import org.apache.jena.sparql.algebra.Op;
-import org.apache.jena.sparql.algebra.op.OpExtend;
-import org.apache.jena.sparql.algebra.op.OpProject;
-import org.apache.jena.sparql.algebra.op.OpTriple;
+import org.apache.jena.sparql.algebra.op.*;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.QueryIterator;
-import org.apache.jena.sparql.engine.binding.BindingBuilder;
-import org.apache.jena.sparql.engine.binding.BindingFactory;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.engine.iterator.QueryIterAssign;
 import org.apache.jena.sparql.engine.iterator.QueryIterPlainWrapper;
+import org.apache.jena.sparql.expr.aggregate.AggCount;
 
 import java.util.Iterator;
 
@@ -57,16 +56,8 @@ public class RawerOpExecutor extends ReturningArgsOpVisitor<Iterator<BindingId2V
     public QueryIterator execute(Op root) {
         root = ReturningOpVisitorRouter.visit(new BGP2Triples(), root); // TODO fix
         execCxt.getContext().set(SagerConstants.SAVER, new Save2SPARQL(root, execCxt));
-
         Iterator<BindingId2Value> wrapped = new RandomRoot(this, execCxt, root);
-
-        return QueryIterPlainWrapper.create(Iter.map(wrapped, bnid -> {
-            BindingBuilder builder = BindingFactory.builder();
-            for (Var var : bnid) {
-                builder.add(var, bnid.getValue(var));
-            }
-            return builder.build();
-        }), execCxt);
+        return biv2qi(wrapped, execCxt);
     }
 
     @Override
@@ -80,7 +71,80 @@ public class RawerOpExecutor extends ReturningArgsOpVisitor<Iterator<BindingId2V
     }
 
     @Override
-    public Iterator<BindingId2Value> visit(OpExtend extend, Iterator<BindingId2Value> args) {
-        return super.visit(extend, args);
+    public Iterator<BindingId2Value> visit(OpExtend extend, Iterator<BindingId2Value> input) {
+        Iterator<BindingId2Value> wrapped = ReturningArgsOpVisitorRouter.visit(this, extend.getSubOp(), input);
+        // TODO implement a QueryIterAssign that returns BindingId2Value directly
+        QueryIterator qi = new QueryIterAssign(biv2qi(wrapped, execCxt), extend.getVarExprList(), execCxt, true);
+        return qi2biv(qi, backend);
     }
+
+    @Override
+    public Iterator<BindingId2Value> visit(OpJoin join, Iterator<BindingId2Value> input) {
+        input = ReturningArgsOpVisitorRouter.visit(this, join.getLeft(), input);
+        return ReturningArgsOpVisitorRouter.visit(this, join.getRight(), input);
+    }
+
+    @Override
+    public Iterator<BindingId2Value> visit(OpTable table, Iterator<BindingId2Value> input) {
+        if (table.isJoinIdentity())
+            return input;
+        throw new UnsupportedOperationException("TODO: VALUES…"); // TODO
+    }
+
+    @Override
+    public Iterator<BindingId2Value> visit(OpGroup groupBy, Iterator<BindingId2Value> input) {
+        // groupVars = Empty
+        // aggregators = List [AggCount (count *), var ?.0, exprvar ?.0]
+        switch (groupBy.getAggregators().get(0).getAggregator()) { // TODO handle the list.
+            case AggCount a -> {System.out.println("TODO");}
+            default -> throw new UnsupportedOperationException("TODO Aggregator "+
+                    groupBy.getAggregators().get(0).getAggregator());
+        }
+        return super.visit(groupBy, input);
+    }
+
+    /* ****************************************************************** */
+
+    /**
+     * @param biv The iterator used in this query executor.
+     * @param ec The execution context of the query.
+     * @return The QueryIterator that is most used in Apache Jena.
+     */
+    public static QueryIterator biv2qi (Iterator<BindingId2Value> biv, ExecutionContext ec) {
+        return QueryIterPlainWrapper.create(Iter.map(biv, bnid -> bnid), ec);
+    }
+
+    /**
+     * @param qi The QueryIterator that is most used in Apache Jena.
+     * @param backend The backend to provide a default table to use to retrieve Node and NodeId.
+     * @return An iterator suited for this engine.
+     */
+    public static Iterator<BindingId2Value> qi2biv(QueryIterator qi, JenaBackend backend) {
+        return new Iterator<BindingId2Value>() {
+            @Override
+            public boolean hasNext() {
+                return qi.hasNext();
+            }
+
+            @Override
+            public BindingId2Value next() {
+                Binding binding = qi.next();
+                return switch (binding) {
+                    case BindingId2Value biv -> biv;
+                    default -> {
+                        // TODO change default table
+                        BindingId2Value biv = new BindingId2Value().setDefaultTable(backend.getNodeTripleTable());
+                        for (Iterator<Var> it = binding.vars(); it.hasNext(); ) {
+                            Var v = it.next();
+                            biv.put(v, binding.get(v));
+                        }
+                        yield biv;
+                    }
+                };
+            }
+        };
+    }
+
+
+
 }
--- rawer/src/test/java/fr/gdd/sage/rawer/RawerOpExecutorTest.java
+++ rawer/src/test/java/fr/gdd/sage/rawer/RawerOpExecutorTest.java
@@ -2,6 +2,7 @@ package fr.gdd.sage.rawer;
 
 import fr.gdd.sage.databases.inmemory.InMemoryInstanceOfTDB2ForRandom;
 import org.apache.jena.query.ARQ;
+import org.apache.jena.query.Dataset;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
@@ -20,34 +21,38 @@ public class RawerOpExecutorTest {
     private static final Logger log = LoggerFactory.getLogger(RawerOpExecutorTest.class);
     private static final InMemoryInstanceOfTDB2ForRandom dataset = new InMemoryInstanceOfTDB2ForRandom();
 
-
     @Test
     public void simple_triple_pattern () { // as per usual
         String queryAsString = "SELECT * WHERE {?s ?p ?o}";
-        Op query = Algebra.compile(QueryFactory.create(queryAsString));
-
-        ExecutionContext ec = new ExecutionContext(dataset.getDataset().asDatasetGraph());
-        ARQ.enableOptimizer(false);
-        RawerOpExecutor executor = new RawerOpExecutor(ec).setLimit(10L);
-
-        QueryIterator iterator = executor.execute(query);
-
-        int nbResults = 0;
-        while (iterator.hasNext()) {
-            log.debug("{}", iterator.next());
-            nbResults += 1;
-        }
-        assertEquals(10, nbResults);
+        execute(queryAsString, dataset.getDataset(), 10L);
     }
 
     @Test
     public void simple_project_on_a_triple_pattern () { // as per usual
         String queryAsString = "SELECT ?s WHERE {?s ?p ?o}";
+        execute(queryAsString, dataset.getDataset(), 10L);
+    }
+
+    @Test
+    public void simple_bind_on_a_triple_pattern () {
+        String queryAsString = "SELECT * WHERE {BIND (<http://Alice> AS ?s) ?s ?p ?o}";
+        execute(queryAsString, dataset.getDataset(), 10L);
+    }
+
+    @Test
+    public void count_of_simple_triple_pattern () {
+        String queryAsString = "SELECT (COUNT(*) AS ?c) WHERE {?s ?p ?o}";
+        execute(queryAsString, dataset.getDataset(), 10L);
+    }
+
+    /* ************************************************************* */
+
+    public static void execute(String queryAsString, Dataset dataset, Long limit) {
         Op query = Algebra.compile(QueryFactory.create(queryAsString));
 
-        ExecutionContext ec = new ExecutionContext(dataset.getDataset().asDatasetGraph());
+        ExecutionContext ec = new ExecutionContext(dataset.asDatasetGraph());
         ARQ.enableOptimizer(false);
-        RawerOpExecutor executor = new RawerOpExecutor(ec).setLimit(10L);
+        RawerOpExecutor executor = new RawerOpExecutor(ec).setLimit(limit);
 
         QueryIterator iterator = executor.execute(query);
 
@@ -56,27 +61,7 @@ public class RawerOpExecutorTest {
             log.debug("{}", iterator.next());
             nbResults += 1;
         }
-        assertEquals(10, nbResults);
+        assertEquals(limit, nbResults);
     }
-
-    @Test
-    public void count_of_simple_triple_pattern () {
-        String queryAsString = "SELECT (COUNT(*) AS ?c) WHERE {?s ?p ?o}";
-        Op query = Algebra.compile(QueryFactory.create(queryAsString));
-
-        ExecutionContext ec = new ExecutionContext(dataset.getDataset().asDatasetGraph());
-        ARQ.enableOptimizer(false);
-        RawerOpExecutor executor = new RawerOpExecutor(ec).setLimit(10L);
-
-        QueryIterator iterator = executor.execute(query);
-
-        int nbResults = 0;
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-            nbResults += 1;
-        }
-        assertEquals(10, nbResults);
-    }
-
 
 }
